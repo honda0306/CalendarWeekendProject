ebb3679eab445b09e9b3fb7ea544062f6b39de66
Jonathan Jamel Holloway <38900224+codejoncode@users.noreply.github.com>
2018-08-04 00:34:02 -0500

    Add files via upload

---
+++ calendar/src/AugustData.js
@@ -0,0 +1,150 @@
+const august = [
+    {
+        date: 1, 
+        day: "Wednesday"
+},
+{
+     date: 2, 
+     day:"Thursday"
+}, 
+{
+    date: 3, 
+    day:"Friday"
+}, 
+ {
+    date: 4,
+    day:  "Saturday"  
+ },
+  {
+    date: 5, 
+    day: "Sunday"
+  }, 
+  {
+    date: 6, 
+    day: "Monday"
+  },
+  {
+    date: 7, 
+    day: "Tuesday"
+  },
+   {
+    date: 8, 
+    day: "Wednesday"
+   }, 
+   {
+    date: 9, 
+    day: "Thursday"
+   }, 
+   {
+    date: 10, 
+    day: "Friday"
+   }, 
+   {
+    date: 11 , 
+    day: "Saturday"
+
+   },
+   {
+    date:12 , 
+    day: "Sunday"
+
+   },
+   {
+    date:13 , 
+    day: "Monday"
+
+   },
+   {
+    date: 14, 
+    day: "Tuesday"
+
+   },
+   {
+    date:15 , 
+    day: "Wednesday"
+
+   },
+   {
+    date: 16, 
+    day: "Thursday"
+
+   },
+   {
+    date:17 , 
+    day: "Friday"
+
+   },
+   {
+    date:18 , 
+    day: "Saturday"
+
+   },
+   {
+    date: 19 , 
+    day: "Sunday"
+
+   },
+   {
+    date: 20 , 
+    day: "Monday"
+
+   },
+   {
+    date: 21 , 
+    day: "Tuesday"
+
+   },
+   {
+    date: 22, 
+    day: "Wednesday"
+
+   },
+   {
+    date: 23, 
+    day: "Thursday"
+
+   },
+   {
+    date: 24, 
+    day: "Friday"
+
+   },
+   {
+    date: 25, 
+    day: "Saturday"
+
+   },
+   {
+    date: 26, 
+    day: "Sunday"
+
+   },
+   {
+    date: 27, 
+    day: "Monday"
+
+   },
+   {
+    date: 28, 
+    day: "Tuesday"
+
+   },
+   {
+    date: 29, 
+    day: "Wednesday"
+
+   },
+   {
+    date: 30, 
+    day: "Thursday"
+
+   },
+   {
+    date: 31, 
+    day: "Friday"
+
+   }
+
+]
+
+export default august; 
